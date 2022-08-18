b6b7f7bb71953968609ad21cad9b88f2cd8784ad
dinixweb <sd4live@gmail.com>
2022-08-18 23:56:46 +0100

    new updates

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,10 +1,10 @@
 import { todo } from "../interfaces/todo";
 
-const InputField = ({ todo, setTodo }:todo) => {
+const InputField = ({ todo, setTodo }: todo) => {
   return (
     <div className="main">
       <span>
-        <input type="text" />
+        <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)} />
         <button className="ms-3 btn btn-primary rounded-pill">Submit</button>
       </span>
     </div>
