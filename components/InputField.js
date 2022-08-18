82b51a817e969f96f38a4266d9c78e62ce87fc01
dinixweb <sd4live@gmail.com>
2022-08-18 23:40:58 +0100

    updates

---
+++ components/InputField.js
@@ -0,0 +1,11 @@
+const InputField = () => {
+  return (
+    <div className="main">
+      <span>
+        <input type="text" />
+        <button className="ms-3 btn btn-primary rounded-pill">Submit</button>
+      </span>
+    </div>
+  );
+};
+export default InputField;
